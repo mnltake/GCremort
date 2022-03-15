0e3485e3959835e3c30036fb7c58035ff46588e6
mnltake <minolu2003@ybb.ne.jp>
2022-03-15 17:57:40 +0900

    seikei

--- GCcontroler/GCcontroler/src/main.cpp
+++ GCcontroler/GCcontroler/src/main.cpp
@@ -24,113 +24,118 @@ uint8_t data[8];
 
 //+++++++++++++++i_to_char+++++++++++++++++
 // i=IntegerValueData,*d=Array pointer, n=Array start No
-void i_to_char(int i,uint8_t *d, int n)
-{
-t=millis();
-d[n] = i & 0x000000ff;
-d[n+1] = (i & 0x0000ff00) >> 8;
-d[n+2] = (i & 0x00ff0000) >> 16;
-d[n+3] = (i & 0xff000000) >> 24;
+void i_to_char(int i,uint8_t *d, int n){
+  t=millis();
+  d[n] = i & 0x000000ff;
+  d[n+1] = (i & 0x0000ff00) >> 8;
+  d[n+2] = (i & 0x00ff0000) >> 16;
+  d[n+3] = (i & 0xff000000) >> 24;
 }
 //++++++++++++++++++++++++++++++++++++++++++
 //=====================================ESP NOW Sub ================================================
-esp_now_peer_info_t slave;// 送信コールバック
+esp_now_peer_info_t slave;
+
+// 送信コールバック
 void OnDataSent(const uint8_t *mac_addr, esp_now_send_status_t status) {
-char macStr[18];
-snprintf(macStr, sizeof(macStr), "%02X:%02X:%02X:%02X:%02X:%02X",
-mac_addr[0], mac_addr[1], mac_addr[2], mac_addr[3], mac_addr[4], mac_addr[5]);}// 受信コールバック
-void OnDataRecv(const uint8_t *mac_addr, const uint8_t *data, int data_len)
-{
-char macStr[18];
-pushN++;
-/*
-Serial.print("M5StickRcved:");
-for (i=0;i<data_len;i++)
-{
-Serial.print(data[i],HEX);
-Serial.print(",");
+  char macStr[18];
+  snprintf(macStr, sizeof(macStr), "%02X:%02X:%02X:%02X:%02X:%02X",
+  mac_addr[0], mac_addr[1], mac_addr[2], mac_addr[3], mac_addr[4], mac_addr[5]);
 }
-Serial.println("");
-//j=0;//reset Data couter
-*/
-}
-void zeroR()
-{
-j=0;
-Serial.println("=========zeroR_j===========");
-}//============================================ESP-NOW Sub end==========================================================================================
-void ESP_error(esp_err_t result)
-{
-Serial.print("Send Status: ");
-if (result == ESP_OK) {
-Serial.println("Success");
-} else if (result == ESP_ERR_ESPNOW_NOT_INIT) {
-Serial.println("ESPNOW not Init.");
-} else if (result == ESP_ERR_ESPNOW_ARG) {
-Serial.println("Invalid Argument");
-} else if (result == ESP_ERR_ESPNOW_INTERNAL) {
-Serial.println("Internal Error");
-} else if (result == ESP_ERR_ESPNOW_NO_MEM) {
-Serial.println("ESP_ERR_ESPNOW_NO_MEM");
-} else if (result == ESP_ERR_ESPNOW_NOT_FOUND) {
-Serial.println("Peer not found.");
-} else {
-Serial.println("Not sure what happened");
-}
-j=0;
-}
-void remocon(){
-M5.update();
-M5.Lcd.fillScreen(BLACK) ;
-M5.Lcd.setTextColor(WHITE);
-M5.Lcd.setTextSize(2);
-M5.Lcd.setCursor(10, 0);
-M5.Lcd.printf("%.2fv%.0fma\r\n", M5.Axp.GetBatVoltage(), M5.Axp.GetBatCurrent());
-M5.Lcd.setTextSize(4);
-M5.Lcd.setCursor(10, 30);
 
-if(M5.BtnA.pressedFor(100)){//kaisoku
-  buttonA++;
-  boolean kai = !(buttonA%2);
-  if (kai){
-    M5.Lcd.setTextColor(ORANGE);
-    M5.Lcd.print("FAST");
-  }else{
-    M5.Lcd.setTextColor(GREEN);
-    M5.Lcd.print("SLOW");
+// 受信コールバック
+void OnDataRecv(const uint8_t *mac_addr, const uint8_t *data, int data_len){
+  char macStr[18];
+  pushN++;
+  /*
+  Serial.print("M5StickRcved:");
+  for (i=0;i<data_len;i++)
+  {
+  Serial.print(data[i],HEX);
+  Serial.print(",");
   }
-  
-  digitalWrite(LED_Pin,kai);
-  i_to_char(0,data,0);
-  i_to_char(buttonA,data,4);
-  esp_err_t result = esp_now_send(slave.peer_addr, data, sizeof(data));
-  delay(500);
-
-}else if(M5.BtnB.wasPressed()){//STOP
-  M5.Lcd.fillScreen(RED) ;
-  M5.Lcd.setTextColor(WHITE);
-  M5.Lcd.print("STOP");
-  i_to_char(3,data,0);
-  esp_err_t result = esp_now_send(slave.peer_addr, data, sizeof(data));
-  delay(500);
-}else if (digitalRead(R_button)==0){//Right
-
-  M5.Lcd.setTextColor(RED);
-  M5.Lcd.print("Right");
-  i_to_char(1,data,0);
-  esp_err_t result = esp_now_send(slave.peer_addr, data, sizeof(data));
-  delay(50);
-}else if (digitalRead(L_button)==0){//Left
-
-  M5.Lcd.setTextColor(BLUE);
-  M5.Lcd.print("Left");
-  i_to_char(2,data,0);
-  esp_err_t result = esp_now_send(slave.peer_addr, data, sizeof(data));
-  delay(50);
-}else{
-  i_to_char(0,data,0);
-  //esp_err_t result = esp_now_send(slave.peer_addr, data, sizeof(data));
+  Serial.println("");
+  //j=0;//reset Data couter
+  */
 }
+
+void zeroR(){
+  j=0;
+  Serial.println("=========zeroR_j===========");
+}
+
+//============================================ESP-NOW Sub end==========================================================================================
+void ESP_error(esp_err_t result){
+  Serial.print("Send Status: ");
+  if (result == ESP_OK) {
+    Serial.println("Success");
+  } else if (result == ESP_ERR_ESPNOW_NOT_INIT) {
+    Serial.println("ESPNOW not Init.");
+  } else if (result == ESP_ERR_ESPNOW_ARG) {
+    Serial.println("Invalid Argument");
+  } else if (result == ESP_ERR_ESPNOW_INTERNAL) {
+    Serial.println("Internal Error");
+  } else if (result == ESP_ERR_ESPNOW_NO_MEM) {
+    Serial.println("ESP_ERR_ESPNOW_NO_MEM");
+  } else if (result == ESP_ERR_ESPNOW_NOT_FOUND) {
+    Serial.println("Peer not found.");
+  } else {
+    Serial.println("Not sure what happened");
+  }
+  j=0;
+}
+
+void remocon(){
+  M5.update();
+  M5.Lcd.fillScreen(BLACK) ;
+  M5.Lcd.setTextColor(WHITE);
+  M5.Lcd.setTextSize(2);
+  M5.Lcd.setCursor(10, 0);
+  M5.Lcd.printf("%.2fV%.0fmA\r\n", M5.Axp.GetBatVoltage(), M5.Axp.GetBatCurrent());
+  M5.Lcd.setTextSize(4);
+  M5.Lcd.setCursor(10, 30);
+
+  if(M5.BtnA.pressedFor(100)){//kaisoku
+    buttonA++;
+    boolean kai = !(buttonA%2);
+    if (kai){
+      M5.Lcd.setTextColor(ORANGE);
+      M5.Lcd.print("FAST");
+    }else{
+      M5.Lcd.setTextColor(GREEN);
+      M5.Lcd.print("SLOW");
+    }
+    
+    digitalWrite(LED_Pin,kai);
+    i_to_char(0,data,0);
+    i_to_char(buttonA,data,4);
+    esp_err_t result = esp_now_send(slave.peer_addr, data, sizeof(data));
+    delay(500);
+
+  }else if(M5.BtnB.wasPressed()){//STOP
+    M5.Lcd.fillScreen(RED) ;
+    M5.Lcd.setTextColor(WHITE);
+    M5.Lcd.print("STOP");
+    i_to_char(3,data,0);
+    esp_err_t result = esp_now_send(slave.peer_addr, data, sizeof(data));
+    delay(500);
+  }else if (digitalRead(R_button)==0){//Right
+
+    M5.Lcd.setTextColor(RED);
+    M5.Lcd.print("Right");
+    i_to_char(1,data,0);
+    esp_err_t result = esp_now_send(slave.peer_addr, data, sizeof(data));
+    delay(50);
+  }else if (digitalRead(L_button)==0){//Left
+
+    M5.Lcd.setTextColor(BLUE);
+    M5.Lcd.print("Left");
+    i_to_char(2,data,0);
+    esp_err_t result = esp_now_send(slave.peer_addr, data, sizeof(data));
+    delay(50);
+  }else{
+    i_to_char(0,data,0);
+    //esp_err_t result = esp_now_send(slave.peer_addr, data, sizeof(data));
+  }
 }
 
 void setup() {
@@ -166,10 +171,11 @@ M5.Lcd.print("ESPNow Init Success\n");
 Serial.println("ESPNow Init Failed");
 M5.Lcd.print("ESPNow Init Failed\n");
 ESP.restart();
-}// マルチキャスト用Slave登録
+}
+// マルチキャスト用Slave登録
 memset(&slave, 0, sizeof(slave));
 for (int i = 0; i < 6; ++i) {
-slave.peer_addr[i] = (uint8_t)0xff;
+  slave.peer_addr[i] = (uint8_t)0xff;
 }
 esp_err_t addStatus = esp_now_add_peer(&slave);
 if (addStatus == ESP_OK) {
